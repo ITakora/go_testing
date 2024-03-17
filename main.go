7f0f5b6129e7f2b44e061a09b76e74107a0573c8
ITakora <oyamitakora@gmail.com>
2024-03-17 16:36:25 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvents)
+
+	server.Run(":8080")
+
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvents(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Errors"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Event Created", "event": event})
+}
